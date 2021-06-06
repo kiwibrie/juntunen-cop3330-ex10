8bf2ae9e18fc86df198e5c303d4339e0c2b0482a
kiwibrie <brianne0128@gmail.com>
2021-06-05 20:39:45 -0500

    Initial commit

---
+++ src/main/java/base/App.java
@@ -0,0 +1,10 @@
+package base;
+
+import java.util.Scanner;
+public class App {
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(){
+        App myApp = new App();
+    }
+}
